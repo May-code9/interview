ac2ea4146e7c0f276fcdf86fd134046c0cc350c6
May-code9 <mayowacode9@gmail.com>
2022-01-24 12:34:10 +0100

    initial commit

--- src/components/MasterLayout.vue
+++ src/components/MasterLayout.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="flex flex-col h-screen justify-between">
-    <div class="overflow-y-auto">
-      <top-layer :title="$attrs.title"></top-layer>
-      <slot></slot>
-    </div>
-    <bottom-layer :title="$attrs.title"></bottom-layer>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import BottomLayer from './BottomLayer.vue'
-import TopLayer from './TopLayer.vue'
-
-export default { components: { TopLayer, BottomLayer } }
+export default {
+  components: {
+    //
+  }
+}
 </script>
 
 <style></style>
